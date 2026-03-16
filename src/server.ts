cbe30c53127bd4eacbbac940a58d354aee41f067
Phillip Thapa <1thapat@gmail.com>
2026-03-16 13:45:49 -0400

    added listener and tested

--- src/server.ts
+++ src/server.ts
@@ -6,9 +6,11 @@ dotenv.config();
 
 const app = express();
 
+const PORT = 3008;
+
 
 app.get("/", (req, res)=>{
   res.send({ fact: "placeholder fact"})
 })
 
-
+app.listen(PORT,() =>{console.log(`Server is running on http://localhost:${PORT}`);})
