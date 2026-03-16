006613edbb928d15fbdd4fe076234004618b66b9
Phillip Thapa <1thapat@gmail.com>
2026-03-16 15:53:57 -0400

    added app.get method to fetch the data from api & used try catch if any errors when fetching

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
+import axios from 'axios';
+
 
 dotenv.config();
 
@@ -10,7 +12,16 @@ const PORT = 3008;
 
 
 app.get("/", (req, res)=>{
-  res.send({ fact: "placeholder fact"})
+  res.send("server is running")
 })
 
+
+app.get("/api/fun-facts", async (req, res)=>{
+  try{
+    const response = await axios.get("https://uselessfacts.jsph.pl/api/v2/facts/random");
+  res.json({ fact: response.data.text });
+  }catch (error){
+    res.status(500).json({error: "could not fetch fun fact"})
+  }
+})
 app.listen(PORT,() =>{console.log(`Server is running on http://localhost:${PORT}`);})
